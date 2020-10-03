43103233e6bb1f115bbdb918c7302ebd2637ff08
zeeshanhanif <zeeshanhanif@gmail.com>
2020-10-03 23:25:39 +0500

    added gatsby site with functions

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,21 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+
+  const [data, setData] = useState("");
+
+  useEffect(()=>{
+    (async()=>{
+        const reponse = await fetch(`.netlify/functions/hello`);
+        const tempData = await reponse.json();
+        setData(tempData);
+    }
+    )();
+  },[]);
+
+
+  return (<div>
+    Hello world!
+    <div>{data.message}</div>
+    </div>)
 }
